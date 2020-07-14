fbc72029de2f6560153d6d9dd516b5ad1d600197
AyloSrd <aylo.srd@gmail.com>
2020-07-14 17:48:26 +0200

    frontend added

--- mod.ts
+++ mod.ts
@@ -1,7 +1,33 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts'
+import { Application, send } from 'https://deno.land/x/oak/mod.ts'
 
 const app = new Application()
 const PORT = 8000
+
+app.use( async (ctx, next) => {
+	await next()
+	const time = ctx.response.headers.get('X-Response-Time')
+	console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`)
+})
+
+app.use( async (ctx, next) => {
+	const start = Date.now()
+	await next()
+	const delta = Date.now() - start
+	ctx.response.headers.set('X-Response-Time', `${delta}ms`)
+})
+
+app.use(async ctx => {
+	const filePath= ctx.request.url.pathname
+	const fileWhitelist = [
+		'/index.html',
+		'/javascripts/script.js',
+		'/stylesheets/style.css',
+		'/images/favicon.png'
+	]
+	await send(ctx, filePath, {
+		root: `${Deno.cwd()}/public`
+	})
+})
 
 app.use(ctx => {
 	ctx.response.body= 'Hello World'
